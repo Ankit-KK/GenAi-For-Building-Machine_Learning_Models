8005aad24e86af6cae8728a4b889b4578ae3d251
Ankit Kumar <ankitashuk20@gmail.com>
2024-11-25 01:14:37 +0530

    Update AnalysisAI.py

--- AnalysisAI.py
+++ AnalysisAI.py
@@ -12,12 +12,13 @@ api_key = "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
 @st.cache_resource
 def get_nvidia_client():
      return ChatNVIDIA(
-          model="meta/llama-3.1-70b-instruct",
-          api_key=api_key, 
-          temperature=0.2,
-          top_p=0.7,
-          max_tokens=4096,
-     )
+  model="meta/llama-3.2-3b-instruct",
+  api_key="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", 
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+)
+
 def dataset_to_string(df):
     """Convert a dataset to a string format suitable for the model."""
     data_sample = df.head().to_string()
